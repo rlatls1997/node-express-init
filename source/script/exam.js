04b430dc5e140bb2ca955ba137cebe79f8863bd2
sinhyuk <rlatls1997@gmail.com>
2021-09-07 23:09:09 +0900

    chore: webpack 설정 및 예제파일 생성
    webpack.config 설정
    기본 예제 파일 생성

---
+++ source/script/exam.js
@@ -0,0 +1,9 @@
+import css from "../css/exam.scss"
+import img from "../images/logo.svg"
+
+let titleTag = document.querySelector("#title")
+let imgTag = document.querySelector("#image")
+
+titleTag.innerHTML = "app installed successfully"
+imgTag.src = img;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { resolve } from "path";
-
-export const entry = "./source/index.js";
-export const output = {
-  mode: "development",
-  path: resolve(__dirname, "dist"),
-  filename: "index_bundle.js",
-};
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  devtool: 'eval-source-map',
   mode: "development",
-  entry: "./source/index.js",
+  entry: {
+    exam: "./source/script/exam.js",
+    //another: "./source/script/another.js",
+  },
   output: {
-    path: resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
+    path: path.resolve("./public"),
+    filename: "[name]_bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: "./",
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
         },
       },
     ],
   },
-  plugins:[
-      new HtmlWebpackPlugin()
-  ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./source/index.html",
+      filename: "./index.html",
+      chunks: ["exam"],
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: "./source/another.html",
+    //   filename: "./another.html",
+    //   chunks: ["another"],
+    // }),
+  ],
 };
